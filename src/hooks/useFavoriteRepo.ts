5b572eb192622ca152e552cb1b1ac16c1a4de2b7
wintory <willyvongga@hotmail.com>
2023-04-23 19:15:53 +0700

    build: add search data

--- src/hooks/useFavoriteRepo.ts
+++ src/hooks/useFavoriteRepo.ts
@@ -1,9 +1,30 @@
+import debounce from 'lodash/debounce';
 import uniqBy from 'lodash/uniqBy';
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 import { FavoriteRepo } from '../types/repository';
 
 const useFavoriteRepo = () => {
   const [favoriteRepo, setFavoriteRepo] = useState<FavoriteRepo[]>([]);
+  const [searchText, setSearchText] = useState<string>('');
+
+  const resultSearchRepo = useMemo(
+    () =>
+      favoriteRepo.filter(v => {
+        if (searchText) {
+          return (
+            v.full_name.includes(searchText) ||
+            v.description.includes(searchText)
+          );
+        }
+
+        return true;
+      }),
+    [searchText, favoriteRepo]
+  );
+
+  const handleSearch = debounce((text: string) => {
+    setSearchText(text);
+  }, 400);
 
   const handleAddFavoriteRepo = (repoData: FavoriteRepo[]) => {
     const result = uniqBy([...favoriteRepo, ...repoData], v => v.id);
@@ -24,6 +45,8 @@ const useFavoriteRepo = () => {
     setFavoriteRepo,
     handleRemoveFavoriteRepo,
     handleAddFavoriteRepo,
+    handleSearch,
+    resultSearchRepo,
   };
 };
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,12 @@
-import { Box, Button, Icon, styled, Typography } from '@mui/material';
+import { Box, Button, Input, styled, Typography } from '@mui/material';
 import { FC, Fragment, useState } from 'react';
 import Card from '../components/Card';
 import PageWrapper from '../components/PageWrapper';
 import RepositoryModal from '../containers/RepositoryModal';
 import useFavoriteRepo from '../hooks/useFavoriteRepo';
 import DeleteIcon from '@mui/icons-material/Delete';
+import SearchIcon from '@mui/icons-material/Search';
+import SearchOffIcon from '@mui/icons-material/SearchOff';
 
 const Wrapper = styled(Box)(() => ({
   display: 'block',
@@ -21,8 +23,13 @@ const AddButton = styled(Button)(({ theme }) => ({
 
 const Home: FC = () => {
   const [isOpenAddRepo, setIsOpenAddRepo] = useState<boolean>(false);
-  const { favoriteRepo, handleAddFavoriteRepo, handleRemoveFavoriteRepo } =
-    useFavoriteRepo();
+  const {
+    favoriteRepo,
+    handleAddFavoriteRepo,
+    handleRemoveFavoriteRepo,
+    resultSearchRepo,
+    handleSearch,
+  } = useFavoriteRepo();
   const hasFavoriteRepo = favoriteRepo.length > 0;
 
   const handleCloseAddRepo = () => {
@@ -39,8 +46,20 @@ const Home: FC = () => {
             variant="outlined"
             color="primary"
           >
-            <Typography variant="subtitle1">Edit Favorite Repo</Typography>
+            <Typography variant="h5">Edit Favorite Repo</Typography>
           </AddButton>
+          {hasFavoriteRepo && (
+            <Box width="100%" pt={2}>
+              <Input
+                startAdornment={
+                  <SearchIcon fontSize="large" sx={{ mr: '0.8rem' }} />
+                }
+                sx={{ width: '100%', fontSize: '1.8rem' }}
+                placeholder="Search by name and description"
+                onChange={e => handleSearch(e.target.value)}
+              />
+            </Box>
+          )}
         </Box>
         <Box
           width="100%"
@@ -51,31 +70,48 @@ const Home: FC = () => {
           textAlign="left"
           mt={2}
         >
-          {hasFavoriteRepo ? (
-            favoriteRepo.map(data => {
-              return (
-                <Fragment key={data.id}>
-                  <Card
-                    disabled={false}
-                    title={data.full_name}
-                    description={data.description}
-                    icon={
-                      <DeleteIcon
-                        color="error"
-                        fontSize="large"
-                        onClick={() => handleRemoveFavoriteRepo(data.id)}
+          {!hasFavoriteRepo && (
+            <Box
+              justifyContent="center"
+              height="30rem"
+              display="flex"
+              alignItems="center"
+            >
+              <Typography variant="h4">No Favorite Repository.</Typography>
+            </Box>
+          )}
+          {hasFavoriteRepo && (
+            <>
+              {resultSearchRepo.length > 0 ? (
+                resultSearchRepo.map(data => {
+                  return (
+                    <Fragment key={data.id}>
+                      <Card
+                        disabled={false}
+                        title={data.full_name}
+                        description={data.description}
+                        icon={
+                          <DeleteIcon
+                            color="error"
+                            fontSize="large"
+                            onClick={() => handleRemoveFavoriteRepo(data.id)}
+                          />
+                        }
                       />
-                    }
-                  />
-                </Fragment>
-              );
-            })
-          ) : (
-            <div>
-              <Typography variant="subtitle1">
-                No Favorite Repository.
-              </Typography>
-            </div>
+                    </Fragment>
+                  );
+                })
+              ) : (
+                <Box
+                  justifyContent="center"
+                  height="30rem"
+                  display="flex"
+                  alignItems="center"
+                >
+                  <Typography variant="h4">No search result.</Typography>
+                </Box>
+              )}
+            </>
           )}
         </Box>
       </Wrapper>
